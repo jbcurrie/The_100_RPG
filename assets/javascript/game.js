af21ea259be864accca4ff32759b8198cd9104db
jbcurrie <biobacker30@gmail.com>
2017-06-29 12:37:32 -0400

    repaired reset logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -54,15 +54,19 @@ function start() {
 	clonePlayers = $(".startPlayers").clone();
 	cloneMessage = $(".getStarted").clone();
 	//selects hero, assigns enemies
-	$(".startPlayers").one("click", "a", pickHero);
+	// $(".startPlayers").one("click", "a", pickHero);
+	pickHero();
 	// assigns defender
-	$(".enemyPlayers").on("click", "a", defenderSelection);
+	// $(".enemyPlayers").on("click", "a", defenderSelection);
+	defenderSelection();
 	//sets HP AP attack parameters and conditions for win/loss
-	$("div").on("click", "button", attack);
+	debugger;
+	// $("button").on("click", attack);
+	attack();
 	//resets the game, reloads the HTML code
 	//current adds back empty divs (not supposed to happen)
 
-	$("div").on("click", "button",restart);
+	restart();
 
 
 	//event.stopPropogation won't stop the click from bubbling up the DOM if the trigger is the body. 
@@ -71,8 +75,8 @@ function start() {
 
 
 function pickHero () {
-	debugger;
-	// $("body").one("click", "a", function (event) {
+	// debugger;
+	$(".startPlayers").one("click", "a", function (event) {
 		// debugger	;
 		// event.stopPropagation();
 		$("a").removeClass("myCharacter");
@@ -112,227 +116,213 @@ function pickHero () {
 				// $(".enemyPlayers").find(".col-lg-3").last().append($(gameEnemies[j]));		
 
 			// }
-	// });	
+	});	
 };
 
 
 function defenderSelection () {
 
-		debugger	;
+		// debugger	;
 		// event.stopPropagation();
-
-	if  (gameDefenders.length === 0) {
-		for (var i = 0; i < gamePlayers.length; i++) {
-		// console.log(i);
-			if (this.closest("a") === gamePlayers.get(i)) {
-				$(gamePlayersClass[i]).closest("a").addClass("defenderCharacter");
-				$(gamePlayersClass[i]).parent().addClass("defender-col");
-				//will push defender 2x on second round
-				for (var j = 0; j < gameEnemies.length; j++) {
-					// no longer splices at correct position following detach function.
-					// debugger;
-					if ($("a.enemyCharacter").get(j) === $("a.defenderCharacter").get(0)) {
-						$(gamePlayersClass[i]).closest("a").removeClass("enemyCharacter");
-						gameEnemies.splice(j,1);
+	$(".enemyPlayers").on("click", "a", function(event) {
+		if  (gameDefenders.length === 0) {
+			for (var i = 0; i < gamePlayers.length; i++) {
+			// console.log(i);
+				if (this.closest("a") === gamePlayers.get(i)) {
+					$(gamePlayersClass[i]).closest("a").addClass("defenderCharacter");
+					$(gamePlayersClass[i]).parent().addClass("defender-col");
+					//will push defender 2x on second round
+					for (var j = 0; j < gameEnemies.length; j++) {
+						// no longer splices at correct position following detach function.
+						// debugger;
+						if ($("a.enemyCharacter").get(j) === $("a.defenderCharacter").get(0)) {
+							$(gamePlayersClass[i]).closest("a").removeClass("enemyCharacter");
+							gameEnemies.splice(j,1);
+						}
 					}
+					// debugger;
+					//switch places with the below for loop to preserve splice? 
+					gameDefenders.push(gamePlayersClass[i]);
+					detachEnemies.push($(gamePlayersClass[i]).closest(".col-lg-3").detach());
+					//empty enemy players div
+					//add back enemies
+					//gameEnemies.pop() acts as an enemy counter. not import that the array value doesn't match the character
+					
+					// for (var j = 0; j < gameEnemies.length; j++) {
+					// 	// no longer splices at correct position following detach function.
+					// 	if ($("a.enemyCharacter").get(j) === $("a.defenderCharacter").get(0)) {
+					// 		gameEnemies.splice(j,1);
+					// 	}
+					// }
 				}
-				// debugger;
-				//switch places with the below for loop to preserve splice? 
-				gameDefenders.push(gamePlayersClass[i]);
-				detachEnemies.push($(gamePlayersClass[i]).closest(".col-lg-3").detach());
-				//empty enemy players div
-				//add back enemies
-				//gameEnemies.pop() acts as an enemy counter. not import that the array value doesn't match the character
-				
-				// for (var j = 0; j < gameEnemies.length; j++) {
-				// 	// no longer splices at correct position following detach function.
-				// 	if ($("a.enemyCharacter").get(j) === $("a.defenderCharacter").get(0)) {
-				// 		gameEnemies.splice(j,1);
-				// 	}
-				// }
+		
 			}
-	
-		}
-		$(".getStarted").empty().append("<h2>Attack when you're ready!</h2>");
+			$(".getStarted").empty().append("<h2>Attack when you're ready!</h2>");
 
 
-		// debugger;
-		if(($("div").hasClass("attackButton") === false)) {
-			var html = "<div class='row text-center attackButton'>" + 
-							"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
-									"<button type='button' class='btn btn-success attackBtn'>Attack!</button>" +
-							"</div>" +
-						"</div>";
-			$(".enemyPlayers").after(html);
-		} else {
-			$(".attackButton").html("<div class='row text-center attackButton'>" + 
-							"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
-									"<button type='button' class='btn btn-success attackBtn'>Attack!</button>" +
-							"</div>" +
-						"</div>");
-		};
-		// debugger;
-		//defender section has empty columns
-		$.each(detachEnemies, function(k,v) {
-			$(".defenderPlayers").append(v);
-		});
-		// for (j in gameDefenders) {
-		// 	$(".defenderPlayers").append("<div class='col-lg-3 col-md-3 col-sm-3 col-xs-3'></div>");
-		// 	$(".defenderPlayers").find(".col-lg-3").last().append($(gameDefenders[j]));
-		// }
+			// debugger;
+			if(($("div").hasClass("attackButton") === false)) {
+				var html = "<div class='row text-center attackButton'>" + 
+								"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
+										"<button type='button' class='btn btn-success attackBtn'>Attack!</button>" +
+								"</div>" +
+							"</div>";
+				$(".enemyPlayers").after(html);
+			} else {
+				$(".attackButton").html("<div class='row text-center attackButton'>" + 
+								"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
+										"<button type='button' class='btn btn-success attackBtn'>Attack!</button>" +
+								"</div>" +
+							"</div>");
+			};
+			// debugger;
+			//defender section has empty columns
+			$.each(detachEnemies, function(k,v) {
+				$(".defenderPlayers").append(v);
+			});
+			// for (j in gameDefenders) {
+			// 	$(".defenderPlayers").append("<div class='col-lg-3 col-md-3 col-sm-3 col-xs-3'></div>");
+			// 	$(".defenderPlayers").find(".col-lg-3").last().append($(gameDefenders[j]));
+			// }
 
-		// debugger;
-	}; 
+			// debugger;
+		}; 
+	});
 
 };
 
 
 function attack () {
 	debugger;
-	// event.stopPropagation();
+	$("div").on("click", "button", function(event) {
+		event.stopPropagation();
+		// $("restartBtn").off("click","button",restart);
+			if (gameDefenders.length > 0 && gameHeroes.length > 0 && $(this).hasClass("attackBtn")) {
+				// on click run attack function
+				myCharacterName = $(gameHeroes[0]).data("name");
 
-		if (gameDefenders.length > 0 && gameHeroes.length > 0 && $(this).hasClass("attackBtn")) {
-			// on click run attack function
-			myCharacterName = $(gameHeroes[0]).data("name");
+				defenderAP = $(gameDefenders[0]).data("ap");
+				defenderName = $(gameDefenders[0]).data("name");
+				var tempArr = gameHeroes[0].split(".");
+				var temp = tempArr.shift();
+				// console.log(tempArr);
+				for (var i = 0; i < gamePlayersClass.length; i++) {
+					if ($(gamePlayersClass[i]).hasClass(tempArr[0])) {
+						// debugger;
+						myCharacterHP = $(gamePlayersClass[i]).data("hp") - defenderAP;
 
-			defenderAP = $(gameDefenders[0]).data("ap");
-			defenderName = $(gameDefenders[0]).data("name");
-			var tempArr = gameHeroes[0].split(".");
-			var temp = tempArr.shift();
-			// console.log(tempArr);
-			for (var i = 0; i < gamePlayersClass.length; i++) {
-				if ($(gamePlayersClass[i]).hasClass(tempArr[0])) {
-					// debugger;
-					myCharacterHP = $(gamePlayersClass[i]).data("hp") - defenderAP;
+						$(gamePlayersClass[i]).data("hp",myCharacterHP);
+						console.log("my character's HP is: " + myCharacterHP);
+						$(gamePlayersHP[i]).find("h4:last").replaceWith($("<h4>HP : " + myCharacterHP + "</h4>"));
 
-					$(gamePlayersClass[i]).data("hp",myCharacterHP);
-					console.log("my character's HP is: " + myCharacterHP);
-					$(gamePlayersHP[i]).find("h4:last").replaceWith($("<h4>HP : " + myCharacterHP + "</h4>"));
+					}
+				}
+				var tempArrE = gameDefenders[0].split(".");
+				var tempE = tempArrE.shift();
 
+				for (var j = 0; j < gamePlayersClass.length; j++) {
+					if ($(gamePlayersClass[j]).hasClass(tempArrE[0])) {
+						myCharacterApAmp += $(gameHeroes[0]).data("ap");
+						defenderHP = $(gamePlayersClass[j]).data("hp") - myCharacterApAmp;
+						$(gamePlayersClass[j]).data("hp",defenderHP);
+						console.log("my character's AP is: " + myCharacterApAmp);
+						console.log("Defender's AP is: " + defenderAP);
+						console.log("defender's HP is: " + defenderHP);
+						$(gamePlayersHP[j]).find("h4:last").replaceWith($("<h4>HP : " + defenderHP + "</h4>"));	
+					}		
 				}
 			}
-			var tempArrE = gameDefenders[0].split(".");
-			var tempE = tempArrE.shift();
-
-			for (var j = 0; j < gamePlayersClass.length; j++) {
-				if ($(gamePlayersClass[j]).hasClass(tempArrE[0])) {
-					myCharacterApAmp += $(gameHeroes[0]).data("ap");
-					defenderHP = $(gamePlayersClass[j]).data("hp") - myCharacterApAmp;
-					$(gamePlayersClass[j]).data("hp",defenderHP);
-					console.log("my character's AP is: " + myCharacterApAmp);
-					console.log("Defender's AP is: " + defenderAP);
-					console.log("defender's HP is: " + defenderHP);
-					$(gamePlayersHP[j]).find("h4:last").replaceWith($("<h4>HP : " + defenderHP + "</h4>"));	
-				}		
+			
+			if ($("button.attackBtn").on("click") && ($("div").hasClass("attackMessage") === false)) {
+				var attackMessage = "<div class='row text-center attackMessage'>" + 
+										"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
+											"<h3>" + defenderName + " attacked you for " + defenderAP + " damage!</h3>" +
+											"<h3>You attacked " + defenderName + " for " + myCharacterApAmp + " damage!</h3>" +
+										"</div>" +
+									"</div>";
+				$(".attackBtn").after(attackMessage);
+			} 
+			else if ($("div").hasClass("attackMessage")) {
+				 // debugger;
+					$(".attackMessage").html("<div class='row text-center attackMessage'>" + 
+										"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
+											"<h3>" + defenderName + " attacked you for " + defenderAP + " damage!</h3>" +
+											"<h3>You attacked " + defenderName + " for " + myCharacterApAmp + " damage!</h3>" +
+										"</div>" +
+									"</div>");
 			}
-		}
-		
-		if ($("button.attackBtn").on("click") && ($("div").hasClass("attackMessage") === false)) {
-			var attackMessage = "<div class='row text-center attackMessage'>" + 
-									"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
-										"<h3>" + defenderName + " attacked you for " + defenderAP + " damage!</h3>" +
-										"<h3>You attacked " + defenderName + " for " + myCharacterApAmp + " damage!</h3>" +
-									"</div>" +
-								"</div>";
-			$(".attackBtn").after(attackMessage);
-		} 
-		else if ($("div").hasClass("attackMessage")) {
-			 // debugger;
+			
+			var reset = "<div class='row text-center restart'>" + 
+							"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
+									"<button type='button' class='btn btn-success restartBtn'>Play Again</button>" +
+							"</div>" +
+						"</div>"
+
+			if (defenderHP < myCharacterHP  && defenderHP < 0 && $("a.enemyCharacter").length !== 0) {
+				$(".getStarted").empty().append("<h2>Choose another Enemy.</h2>");
 				$(".attackMessage").html("<div class='row text-center attackMessage'>" + 
-									"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
-										"<h3>" + defenderName + " attacked you for " + defenderAP + " damage!</h3>" +
-										"<h3>You attacked " + defenderName + " for " + myCharacterApAmp + " damage!</h3>" +
-									"</div>" +
+								"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
+									"<h3> You've defeated " + defenderName + " </h3>" +
+									"</div>" + 
 								"</div>");
-		}
-		
-		var reset = "<div class='row text-center restart'>" + 
-						"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
-								"<button type='button' class='btn btn-success restartBtn'>Play Again</button>" +
-						"</div>" +
-					"</div>"
 
-		if (defenderHP < myCharacterHP  && defenderHP < 0 && $("a.enemyCharacter").length !== 0) {
-			$(".getStarted").empty().append("<h2>Choose another Enemy.</h2>");
-			$(".attackMessage").html("<div class='row text-center attackMessage'>" + 
-							"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
-								"<h3> You've defeated " + defenderName + " </h3>" +
-								"</div>" + 
-							"</div>");
+				//warps my point system. need to fix
+				detachDefenders.push($("a.defenderCharacter").closest(".col-lg-3").detach());
+				$("a.defenderCharacter").remove();
+				detachEnemies.shift();
+				gameDefenders.pop();
+				// debugger;
+			} else if (defenderHP < myCharacterHP  && defenderHP < 0 && $("a.enemyCharacter").length === 0) {
+				$(".getStarted").empty();
+				$(".attackMessage").html("<div class='row text-center attackMessage'>" + 
+								"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
+									"<h3> You've defeated " + defenderName + " </h3>" +
+									"</div>" + 
+								"</div>");
 
-			//warps my point system. need to fix
-			detachDefenders.push($("a.defenderCharacter").closest(".col-lg-3").detach());
-			$("a.defenderCharacter").remove();
-			detachEnemies.shift();
-			gameDefenders.pop();
-			// debugger;
-		} else if (defenderHP < myCharacterHP  && defenderHP < 0 && $("a.enemyCharacter").length === 0) {
-			$(".getStarted").empty();
-			$(".attackMessage").html("<div class='row text-center attackMessage'>" + 
-							"<div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>" + 
-								"<h3> You've defeated " + defenderName + " </h3>" +
-								"</div>" + 
-							"</div>");
-
-			//warps my point system. need to fix
-			detachDefenders.push($("a.defenderCharacter").closest(".col-lg-3").detach());
-			$("a.defenderCharacter").remove();
-			detachEnemies.shift();
-			gameDefenders.pop();
-		} else if (myCharacterHP <= 0 && defenderHP > myCharacterHP && $("a.defenderCharacter").length !== 0) {
-			$(".getStarted").empty().append("<h2> You lost! " + defenderName + " Wins! </h2>");
-			gameHeroes.pop();
-			debugger;
-			if ($("div.restart").hasClass("restart") === false) {
-					$(".attackButton").empty().html(reset);
-					$("div.attackButton").removeClass("attackButton");
+				//warps my point system. need to fix
+				detachDefenders.push($("a.defenderCharacter").closest(".col-lg-3").detach());
+				$("a.defenderCharacter").remove();
+				detachEnemies.shift();
+				gameDefenders.pop();
+			} else if (myCharacterHP <= 0 && defenderHP > myCharacterHP && $("a.defenderCharacter").length !== 0) {
+				$(".getStarted").empty().append("<h2> You lost! " + defenderName + " Wins! </h2>");
+				gameHeroes.pop();
+				debugger;
+				if ($("div.restart").hasClass("restart") === false) {
+						$(".attackButton").empty().replaceWith(reset);
+						$("div.attackButton").removeClass("attackButton");
+				}
 			}
-		}
 
 
-		// debugger;
-		// debugger;
-		if (gameDefenders.length === 0 && gameEnemies.length === 0 && ($("a").hasClass("enemyCharacter") === false && $("a").length<=1)) {
-			$(".getStarted").empty().append("<h1>You Win!</h1>");
-			$(".attackMessage").remove();
-			$("div.attackButton").empty().html(reset)
-			$("div.attackButton").removeClass("attackButton");
-		}
+			// debugger;
+			// debugger;
+			if (gameDefenders.length === 0 && gameEnemies.length === 0 && ($("a").hasClass("enemyCharacter") === false && $("a").length<=1)) {
+				$(".getStarted").empty().append("<h1>You Win!</h1>");
+				$(".attackMessage").remove();
+				$("div.attackButton").empty().replaceWith(reset)
+				$("div.attackButton").removeClass("attackButton");
+			}
 
-		// if ((gameHeroes.length === 0)) {
-		// 	if ($("div.restart").hasClass("restart") === false) {
-		// 			$(".attackButton").empty().html(reset);
-		// 			$("div.attackButton").removeClass("attackButton");
-		// 	}
-
-
-		// }
-		// duplicate line of code to the above enemy condition
-		// if (($("a.enemyCharacter").length === 0) && ($("a.defenderCharacter").length === 0) && ($("a").length<=1)) {
-		// 	// debugger;
-		// 	if ($("div.restart").hasClass("restart") === false) {
-		// 			$(".attackButton").empty().html(reset);
-		// 			$("div.attackButton").removeClass("attackButton");
-		// 	}
-		// }
-
-		//if enemy array.length = 0, you win. ***pop up (modal) mycharacter gif. 
-		//if hero array.length = 0 OR if hero hp <=0 you lose ***pop up (modal) defender gif.
+			//if enemy array.length = 0, you win. ***pop up (modal) mycharacter gif. 
+			//if hero array.length = 0 OR if hero hp <=0 you lose ***pop up (modal) defender gif.
+	});
 
 }
 
 function restart () {
 	debugger;
-	// event.stopPropagation();
 	//remove event listeners at reset
-
+	$("div").on("click", "button",function(event) {
+		event.stopImmediatePropagation();
 		// debugger	;
 		// event.stopPropagation();
 		if ($(this).hasClass("restartBtn")) {
 			//empty the start players
 			$(".enemyPlayers").off("click","a",defenderSelection); // removeEventListener("click", defenderSelection);
-			$(".attackBtn").off("click","button",attack); //removeEventListener("click", attack);
-			$("restartBtn").off("click","button",restart); //removeEventListener("click", restart);
+			$("div").off("click","button",attack); //removeEventListener("click", attack);
+			$("div").off("click","button",restart); //removeEventListener("click", restart);
 			$(gamePlayersClass).empty();
 			gamePlayersClass.splice(0);
 			$(".startPlayers").empty();
@@ -341,7 +331,7 @@ function restart () {
 			$(".enemyPlayers").splice(0);
 			$(".defenderPlayers").empty();
 			$(".defenderPlayers").splice(0);
-			$("div.restart").empty();
+			$("div.restart").remove();
 			// $("a").empty();
 			// $("a").splice(0);
 			//add back start players
@@ -373,7 +363,7 @@ function restart () {
 			clonePlayers;
 			cloneMessage;
 		}
-
+	});
 }
 
 $(document).ready(function() {
